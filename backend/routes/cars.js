8836c8f03239b4918ff054567e11c847db59d21c
maximpollak <cc241059@fhstp.ac.at>
2025-06-11 14:38:32 +0200

    routes fix

--- backend/routes/cars.js
+++ backend/routes/cars.js
@@ -2,10 +2,15 @@ const express = require('express')
 const router = express.Router()
 const carController = require('../controllers/carsController')
 
+//GET ALL CARS
 router.get('/', carController.getAllCars) // public
+//GET ONE CAR
 router.get('/:id', carController.getCarById)
-router.post('/addcar', carController.addCar)    // admin-only later
-router.put('/:id/editcar', carController.editCar)
-router.delete('/:id/remove', carController.deleteCar)
+//ADD A CAR TO RENT
+router.post('/add', carController.addCar)    // admin-only later
+//EDIT A CAR
+router.put('/:id/', carController.editCar)
+//DELETE A CAR
+router.delete('/:id/', carController.deleteCar)
 
 module.exports = router
